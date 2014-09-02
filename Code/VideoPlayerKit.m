5810cd6a543c978a22471b90fe27daed1e094c95
Valentine Silvansky <v.silvansky@gmail.com>
2014-09-02 17:54:38 +0400

    Remove share code

--- Code/VideoPlayerKit.m
+++ Code/VideoPlayerKit.m
@@ -2,7 +2,6 @@
 
 #import "VideoPlayerKit.h"
 #import "FullScreenViewController.h"
-#import "ShareThis.h"
 
 NSString * const kVideoPlayerVideoChangedNotification = @"VideoPlayerVideoChangedNotification";
 NSString * const kVideoPlayerWillHideControlsNotification = @"VideoPlayerWillHideControlsNotitication";
@@ -174,7 +173,6 @@ NSString * const kTrackEventVideoComplete = @"Video Complete";
 - (void)presentShareOptions
 {
     showShareOptions = NO;
-    [ShareThis showShareOptionsToShareUrl:[_currentVideoInfo objectForKey:@"shareURL"] title:[_currentVideoInfo objectForKey:@"title"] image:nil onViewController:[[[UIApplication sharedApplication] keyWindow] rootViewController] forTypeOfContent:STContentTypeVideo];
 }
 
 - (void)shareButtonHandler
